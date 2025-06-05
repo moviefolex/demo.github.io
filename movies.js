5a337209d2d3faa2c14af49d63ba9ac79afd9829
movie folex <freelancingwebsite41@gmail.com>
2025-06-05 12:21:38 +0530

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,32 @@
+const movies = [
+  {
+    id: 1,
+    title: "Inception",
+    poster: "https://m.media-amazon.com/images/I/51eQYiFhPWL._AC_.jpg",
+    language: "English",
+    resolution: "1080p",
+    genre: "Sci-Fi",
+    review: "A mind-bending thriller by Christopher Nolan.",
+    download: "https://example.com/inception-download"
+  },
+  {
+    id: 2,
+    title: "The Royals",
+    poster: "https://m.media-amazon.com/images/M/MV5BODJjZjgxNTctMzYzNS00ZjQ1LThhZmUtMjViMjgxNzI4MjU0XkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg",
+    language: "Hindi",
+    resolution: "720p",
+    genre: "Drama",
+    review: "A gripping story of royal family secrets.",
+    download: "https://example.com/the-royals-download"
+  },
+  {
+    id: 3,
+    title: "Poker Face – S02",
+    poster: "https://m.media-amazon.com/images/M/MV5BNzAzZDdiZDAtYjZmOS00NGE1LWJlZjAtZjAzYTZhZjk4OWZjXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg",
+    language: "English",
+    resolution: "1080p",
+    genre: "Mystery",
+    review: "Season 2 brings more suspense and intrigue.",
+    download: "https://example.com/poker-face-download"
+  }
+];
